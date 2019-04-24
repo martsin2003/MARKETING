2cdce521022fec2da0efeb6cb4a4982ea2db3397
Chris <chris.take@venturplex.com>
2019-04-23 21:21:06 -0700

    feat: community microsite components w/ module

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { FindMyHomeComponent } from './find-my-home/find-my-home.component';
+import { HomePlansComponent } from './home-plans/home-plans.component';
+import { EmpowerInnovationsComponent } from './empower-innovations/empower-innovations.component';
+import { WhyBrookfieldComponent } from './why-brookfield/why-brookfield.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { CommunityMicrositeComponent } from './community-microsite/community-microsite.component';
 
 @Component({
   selector: 'brookfield-root',
@@ -7,4 +14,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'brookfield';
+
+  links = [
+    { path: '', component: HomeComponent, label: 'Home' },
+    { path: 'find-my-home', component: FindMyHomeComponent, label: 'Find My Home' },
+    { path: 'home-plans', component: HomePlansComponent, label: 'Home Plans' },
+    { path: 'empower-innovations', component: EmpowerInnovationsComponent, label: 'Empower Innovations' },
+    { path: 'why', component: WhyBrookfieldComponent, label: 'Why Brookfield' },
+    { path: 'my-account', component: MyAccountComponent, label: 'My Account' },
+    { path: 'community-microsite', component: CommunityMicrositeComponent, label: 'Community Microsite' },
+  ];
 }
