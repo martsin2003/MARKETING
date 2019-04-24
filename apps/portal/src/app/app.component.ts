9d18be26652f0bb644ed7d7605202e6ee44dbfda
Joshua Jones <joshua.jones@venturplex.com>
2019-04-24 14:34:28 -0700

    remove component imports and properties

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { FindMyHomeComponent } from './find-my-home/find-my-home.component';
-import { HomePlansComponent } from './home-plans/home-plans.component';
-import { EmpowerInnovationsComponent } from './empower-innovations/empower-innovations.component';
-import { WhyBrookfieldComponent } from './why-brookfield/why-brookfield.component';
-import { MyAccountComponent } from './my-account/my-account.component';
-import { CommunityMicrositeComponent } from './community-microsite/community-microsite.component';
 
 @Component({
   selector: 'brookfield-root',
@@ -16,12 +9,12 @@ export class AppComponent {
   title = 'brookfield';
 
   links = [
-    { path: '', component: HomeComponent, label: 'Home' },
-    { path: 'find-my-home', component: FindMyHomeComponent, label: 'Find My Home' },
-    { path: 'home-plans', component: HomePlansComponent, label: 'Home Plans' },
-    { path: 'empower-innovations', component: EmpowerInnovationsComponent, label: 'Empower Innovations' },
-    { path: 'why', component: WhyBrookfieldComponent, label: 'Why Brookfield' },
-    { path: 'my-account', component: MyAccountComponent, label: 'My Account' },
-    { path: 'community-microsite', component: CommunityMicrositeComponent, label: 'Community Microsite' },
+    { path: '', label: 'Home' },
+    { path: 'find-my-home', label: 'Find My Home' },
+    { path: 'home-plans', label: 'Home Plans' },
+    { path: 'empower-innovations', label: 'Empower Innovations' },
+    { path: 'why', label: 'Why Brookfield' },
+    { path: 'my-account', label: 'My Account' },
+    { path: 'community-microsite', label: 'Community Microsite' },
   ];
 }
