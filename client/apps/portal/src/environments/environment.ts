6815428caaf133b6f13e65e9fd48c96c42e78fdc
volodymyr-kukurudz <kukurudz.volodymyr@gmail.com>
2019-05-14 20:05:41 +0300

    add backend endpoint to environment variables

--- client/apps/portal/src/environments/environment.ts
+++ client/apps/portal/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backendEndpoint: 'http://bf-dev.us-west-2.elasticbeanstalk.com/',
+  backendGraphqlEndpoint: 'http://bf-dev.us-west-2.elasticbeanstalk.com/graphql'
 };
 
 /*
