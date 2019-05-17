d33809fc4afd303dc35e70232d925c9f04b7c9f1
Myroslav Martsin <forvaro@gmail.com>
2019-05-17 15:59:08 +0300

    create first part of desktop UI

--- client/libs/portal/feature/community/src/lib/portal-feature-community.module.ts
+++ client/libs/portal/feature/community/src/lib/portal-feature-community.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CommunityMicrositeComponent } from './containers/community-microsite/community-microsite.component';
+import { CommonUiComponentsImageCarouselModule } from '@brookfield/common/ui-components/image-carousel';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    CommonUiComponentsImageCarouselModule,
     RouterModule.forChild([{ path: '', pathMatch: 'full', component: CommunityMicrositeComponent }])
   ],
   declarations: [CommunityMicrositeComponent]
