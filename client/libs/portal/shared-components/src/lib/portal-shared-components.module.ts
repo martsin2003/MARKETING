a64f086af36aa65b232a81cccec57b926c08470b
Roman <lead_n_go@yahoo.com>
2019-05-21 07:47:19 +0300

    move community-card to containers

--- client/libs/portal/shared-components/src/lib/portal-shared-components.module.ts
+++ client/libs/portal/shared-components/src/lib/portal-shared-components.module.ts
@@ -1,7 +1,7 @@
 import { MaterialModule } from '@brookfield/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CommunityCardComponent } from './community-card/community-card.component';
+import { CommunityCardComponent } from './containers/community-card/community-card.component';
 import { ViewFilterComponent } from './containers/view-filter/view-filter.component';
 import { DesktopViewFilterComponent } from './presentational/desktop-view-filter/desktop-view-filter.component';
 import { MobileViewFilterComponent } from './presentational/mobile-view-filter/mobile-view-filter.component';
