b2266fde896848b677f4c8000cf1d7df0107230e
Roman <lead_n_go@yahoo.com>
2019-05-21 19:11:53 +0300

    PR review - use mobile view detection service | simplify text substitution in template

--- client/libs/portal/feature/community/src/lib/containers/community-microsite/community-microsite.component.ts
+++ client/libs/portal/feature/community/src/lib/containers/community-microsite/community-microsite.component.ts
@@ -1,22 +1,14 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+import { DetectMobileViewService } from '@brookfield/common/utilities';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'brookfield-community-microsite',
   templateUrl: './community-microsite.component.html',
   styleUrls: ['./community-microsite.component.scss']
 })
-export class CommunityMicrositeComponent implements OnInit, AfterViewInit {
-
-  @ViewChild('description') description: ElementRef<HTMLDivElement>;
-
-  desrciptionText = `Innovative design meets relaxed Loudoun County lakefront style — all at a price that ensures
-  this opportunity won't last long. Make the morning coffee in your spacious, gourmet kitchen. Sit
-  on your front porch and savor each sip. Spend the day kayaking or paddleboarding. Cast a line or
-  pack a picnic. This community is full of outdoor fun in the Loudoun countryside. Hike the trails
-  amid Virginia's stunning natural beauty. This is home.`;
-  readLess: boolean;
-
+export class CommunityMicrositeComponent implements OnInit, OnDestroy {
   communityImages = [
     'assets/images/desktop/cm-slider-second-image.png',
     'assets/images/desktop/cm-slider-first-image.png'
@@ -28,31 +20,47 @@ export class CommunityMicrositeComponent implements OnInit, AfterViewInit {
     'assets/images/desktop/cm-second-slider-third-img.png'
   ];
 
-  constructor() { }
+  private fullDesrciptionText = `Innovative design meets relaxed Loudoun County lakefront style — all at a price that ensures
+  this opportunity won't last long. Make the morning coffee in your spacious, gourmet kitchen. Sit
+  on your front porch and savor each sip. Spend the day kayaking or paddleboarding. Cast a line or
+  pack a picnic. This community is full of outdoor fun in the Loudoun countryside. Hike the trails
+  amid Virginia's stunning natural beauty. This is home.`;
 
-  ngOnInit() { }
+  private trimmedDescriptionText = `${this.fullDesrciptionText.slice(0, 127).trim()}...`;
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+  desrciptionText: string;
+  readLess: boolean;
+  isMobileView$: Observable<boolean>;
 
-  ngAfterViewInit() {
-    fromEvent(window, 'resize').subscribe(() => {
-      this.setDescriptionValuesLength();
-    });
-    this.setDescriptionValuesLength();
+  constructor(private detectMobileViewService: DetectMobileViewService) {}
+
+  ngOnInit() {
+    this.isMobileView$ = this.detectMobileViewService.isMobileView();
+    this.setUpDescriptionChange();
   }
 
-  setDescriptionValuesLength() {
-    if (window.outerWidth <= 500) {
-      this.description.nativeElement.textContent = `${this.desrciptionText.slice(0, 127).trim()}...`;
-    } else {
-      this.description.nativeElement.textContent = this.desrciptionText;
-    }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  private setUpDescriptionChange() {
+    return this.isMobileView$
+      .pipe(
+        map(viewIsMobile => {
+          console.log(1111, viewIsMobile);
+          this.desrciptionText = viewIsMobile
+            ? this.trimmedDescriptionText
+            : this.fullDesrciptionText;
+          this.readLess = false;
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 
   readMore() {
     this.readLess = !this.readLess;
-    if (this.description.nativeElement.textContent.length <= 130) {
-      this.description.nativeElement.textContent = this.desrciptionText;
-    } else {
-      this.description.nativeElement.textContent = `${this.desrciptionText.slice(0, 127).trim()}...`;
-    }
+    this.desrciptionText = this.readLess ? this.fullDesrciptionText : this.trimmedDescriptionText;
   }
 }
