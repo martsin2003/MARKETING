4ef119ed21f5bc2f2d7de0a6c3b1ae2b2d18cd84
Myroslav Martsin <forvaro@gmail.com>
2019-05-21 17:11:34 +0300

    almost finished mobile UI

--- client/libs/portal/feature/community/src/lib/containers/community-microsite/community-microsite.component.ts
+++ client/libs/portal/feature/community/src/lib/containers/community-microsite/community-microsite.component.ts
@@ -1,18 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'brookfield-community-microsite',
   templateUrl: './community-microsite.component.html',
   styleUrls: ['./community-microsite.component.scss']
 })
-export class CommunityMicrositeComponent implements OnInit {
+export class CommunityMicrositeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('description') description: ElementRef<HTMLDivElement>;
+
+  desrciptionText = `Innovative design meets relaxed Loudoun County lakefront style — all at a price that ensures
+  this opportunity won't last long. Make the morning coffee in your spacious, gourmet kitchen. Sit
+  on your front porch and savor each sip. Spend the day kayaking or paddleboarding. Cast a line or
+  pack a picnic. This community is full of outdoor fun in the Loudoun countryside. Hike the trails
+  amid Virginia's stunning natural beauty. This is home.`;
+  readLess: boolean;
 
   communityImages = [
     'assets/images/desktop/cm-slider-second-image.png',
     'assets/images/desktop/cm-slider-first-image.png'
   ];
 
-  constructor() {}
+  secondSliderCommynityImages = [
+    'assets/images/desktop/cm-second-slider-first-img.png',
+    'assets/images/desktop/cm-second-slider-second-img.png',
+    'assets/images/desktop/cm-second-slider-third-img.png'
+  ];
 
-  ngOnInit() {}
+  constructor() { }
+
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    fromEvent(window, 'resize').subscribe(() => {
+      this.setDescriptionValuesLength();
+    });
+    this.setDescriptionValuesLength();
+  }
+
+  setDescriptionValuesLength() {
+    if (window.outerWidth <= 500) {
+      this.description.nativeElement.textContent = `${this.desrciptionText.slice(0, 127).trim()}...`;
+    } else {
+      this.description.nativeElement.textContent = this.desrciptionText;
+    }
+  }
+
+  readMore() {
+    this.readLess = !this.readLess;
+    if (this.description.nativeElement.textContent.length <= 130) {
+      this.description.nativeElement.textContent = this.desrciptionText;
+    } else {
+      this.description.nativeElement.textContent = `${this.desrciptionText.slice(0, 127).trim()}...`;
+    }
+  }
 }
--- client/libs/portal/feature/community/src/lib/portal-feature-community.module.ts
+++ client/libs/portal/feature/community/src/lib/portal-feature-community.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CommunityMicrositeComponent } from './containers/community-microsite/community-microsite.component';
+import { MaterialModule } from '@brookfield/material';
 
 @NgModule({
   imports: [
     CommonModule,
     PortalSharedComponentsModule,
+    MaterialModule,
     RouterModule.forChild([{ path: '', pathMatch: 'full', component: CommunityMicrositeComponent }])
   ],
   declarations: [CommunityMicrositeComponent]
 })
-export class PortalFeatureCommunityModule {}
+export class PortalFeatureCommunityModule { }
