8b2eb3f30ffd9be83de4aef2a51964400f216968
Roman <lead_n_go@yahoo.com>
2019-05-21 09:09:53 +0300

    remove feature-navigation lib (content moved to shared-components)

--- client/libs/portal/shared/feature-navigation/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'portal-shared-feature-navigation',
-  preset: '../../../../jest.config.js',
-  coverageDirectory: '../../../../coverage/libs/portal/shared/feature-navigation',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
