6ff9d61b4121276841b0f185787f634463bceb56
Roman <lead_n_go@yahoo.com>
2019-05-24 12:52:40 +0300

    finish common button styles

--- client/libs/portal/feature/empower/src/lib/portal-feature-empower.module.ts
+++ client/libs/portal/feature/empower/src/lib/portal-feature-empower.module.ts
@@ -4,8 +4,6 @@ import { RouterModule } from '@angular/router';
 import { EmpowerOverviewComponent } from './containers/empower-overview/empower-overview.component';
 import { MaterialModule } from '@brookfield/material';
 import { InnovationVideosComponent } from './presentational/innovation-videos/innovation-videos.component';
-import { MyTimeTourComponent } from './containers/my-time-tour/my-time-tour.component';
-import { MyTimeTourDesktopComponent } from './presentational/my-time-tour-desktop/my-time-tour-desktop.component';
 
 @NgModule({
   imports: [
@@ -19,21 +17,12 @@ import { MyTimeTourDesktopComponent } from './presentational/my-time-tour-deskto
             path: '',
             pathMatch: 'full',
             component: EmpowerOverviewComponent
-          },
-          {
-            path: 'my-time-tour',
-            component: MyTimeTourComponent
           }
         ]
       },
       { path: '**', redirectTo: '' }
     ])
   ],
-  declarations: [
-    EmpowerOverviewComponent,
-    InnovationVideosComponent,
-    MyTimeTourComponent,
-    MyTimeTourDesktopComponent
-  ]
+  declarations: [EmpowerOverviewComponent, InnovationVideosComponent]
 })
 export class PortalFeatureEmpowerModule {}
