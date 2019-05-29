50683851042d4bd82669237c9a5524e847d67bc1
Joshua Jones <joshua.jones@venturplex.com>
2019-05-29 13:40:17 -0700

    BR-177:Create layout styles for my home tours mobile

--- client/libs/portal/feature/my-account/src/lib/presentational/home-tour-card/home-tour-card.component.ts
+++ client/libs/portal/feature/my-account/src/lib/presentational/home-tour-card/home-tour-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HomeTourCardComponent implements OnInit {
   @Input() previus: boolean;
+  @Input() moveInNow: boolean;
 
   constructor() {}
 
