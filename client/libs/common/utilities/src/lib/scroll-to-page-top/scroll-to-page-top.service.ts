f774d847dc2cf3a781b15ee2c5c6ae9e36621290
Roman <lead_n_go@yahoo.com>
2019-05-30 15:47:49 +0300

    scroll to top of page on router navigation

---
+++ client/libs/common/utilities/src/lib/scroll-to-page-top/scroll-to-page-top.service.ts
@@ -0,0 +1,27 @@
+import { filter } from 'rxjs/operators';
+import { Router, Event, Scroll } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrollToPageTopService {
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {}
+
+  scrollTopOnNavtigation() {
+    this.router.events.pipe(filter((e: Event): e is Scroll => e instanceof Scroll)).subscribe(e => {
+      if (e.position) {
+        // backward navigation
+        // this.viewportScroller.scrollToPosition(e.position); - Enable this if need to scroll to previous position
+        this.viewportScroller.scrollToPosition([0, 0]);
+      } else if (e.anchor) {
+        // anchor navigation
+        this.viewportScroller.scrollToAnchor(e.anchor);
+      } else {
+        // forward navigation
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
+}
