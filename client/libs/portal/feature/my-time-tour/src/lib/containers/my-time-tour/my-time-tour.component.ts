71c0d39d6893a2be6d5d40321ef9c4aded8ef59f
Myroslav Martsin <forvaro@gmail.com>
2019-06-03 17:10:48 +0300

    fix my-time-tour layout

--- client/libs/portal/feature/my-time-tour/src/lib/containers/my-time-tour/my-time-tour.component.ts
+++ client/libs/portal/feature/my-time-tour/src/lib/containers/my-time-tour/my-time-tour.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-time-tour.component.scss']
 })
 export class MyTimeTourComponent implements OnInit {
-  step = 0;
+  step = 2;
   isMobileView$: Observable<boolean>;
   constructor(private detectMobileViewService: DetectMobileViewService, private router: Router) {}
 
